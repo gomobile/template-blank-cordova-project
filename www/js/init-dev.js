bf25a38a7b111c216192cb7709b35b332afa1272
xmnboy <xmnboy@yahoo.com>
2014-08-24 21:52:10 -0700

    update copyright notice

--- www/js/init-dev.js
+++ www/js/init-dev.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, Paul Fischer, Intel Corporation. All rights reserved.
+ * Copyright (c) 2013-2014, Intel Corporation. All rights reserved.
  * Please see http://software.intel.com/html5/license/samples for license terms and conditions.
  */
 
