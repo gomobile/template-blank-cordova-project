6e874b976e1af52322743c89eed05a9bc5372d3e
xmnboy <xmnboy@yahoo.com>
2014-08-26 15:05:10 -0700

    copyright and comment updates

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,17 @@
 /*
- * Copyright (c) 2013-2014, Intel Corporation. All rights reserved.
+ * Copyright (c) 2014, Intel Corporation. All rights reserved.
  * Please see http://software.intel.com/html5/license/samples
  * and the included README.md file for license terms and conditions.
  */
 
 
 /*jslint browser:true, devel:true, white:true, vars:true */
-/*global $:false, intel:false app:false, dev:false */
-/*global cordova:false */
+/*global $:false, intel:false app:false, dev:false, cordova:false */
 
 
 
 // This file contains your event handlers, the center of your application.
-
+// See app.initEvents() in init-app.js for event initialization.
 
 function myEventHandler() {
     "use strict" ;
